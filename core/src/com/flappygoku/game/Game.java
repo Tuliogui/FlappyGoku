878a2d486938c2f0adb0f64c7678dd38324c7a27
Túlio Santos <tulioh_@hotmail.com>
2022-03-22 22:19:43 -0300

    Fim de projeto

--- core/src/com/flappygoku/game/Game.java
+++ core/src/com/flappygoku/game/Game.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -13,6 +14,8 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import java.util.Random;
 
@@ -25,7 +28,7 @@ public class Game extends ApplicationAdapter {
 	private Texture torreBaixo;
 	private Texture torreCima;
 	private Texture gameOver;
-	private Texture coin;
+	private Texture play;
 
 	//Formas para colisão
 	private ShapeRenderer shapeRenderer;
@@ -49,13 +52,14 @@ public class Game extends ApplicationAdapter {
 	private boolean passouMoeda = false;
 	private int estadoJogo = 0;
 	private float posiçaoHorizontalGoku = 0;
-	private float posicaoMoeda =0;
 
 
 	//Exibi texto
 	BitmapFont textoPontuaçao;
 	BitmapFont textoReiniciar;
 	BitmapFont textoMelhorPontuaçao;
+	BitmapFont textoIniciar;
+
 
 	//Configuração dos sons
 	Sound somVoando;
@@ -66,12 +70,10 @@ public class Game extends ApplicationAdapter {
 	Preferences preferencia;
 
 	//Objeto para camera
-	/*private OrthographicCamera camera;
+	private OrthographicCamera camera;
 	private Viewport viewport;
 	private final float VIRTUAL_WIDTH = 720;
 	private  final float VIRTUAL_HEIGHT = 1280;
-
-	 */
 
 	@Override
 	public void create () {
@@ -118,10 +120,7 @@ public class Game extends ApplicationAdapter {
 				posicaoTorreVertical = random.nextInt(1000) - 400;
 				passouTorre = false;
 			}
-
-
-
-			// Aplicando gravidade no Bakugou
+			// Aplicando gravidade no Goku
 			if (posicaoInicialVerticalGoku > 0 || toqueTela)
 				posicaoInicialVerticalGoku = posicaoInicialVerticalGoku - gravidade;
 
@@ -132,6 +131,8 @@ public class Game extends ApplicationAdapter {
 			if (pontos > pontuacaoMaxima){
 				pontuacaoMaxima = pontos;
 				preferencia.putInteger("pontuaçaoMaxima", pontuacaoMaxima);
+
+				preferencia.flush();
 
 			}
 
@@ -154,6 +155,20 @@ public class Game extends ApplicationAdapter {
 
 
 	}
+
+	/*
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer.setColor(Color.RED);
+		shapeRenderer.circle(30 + gokus[0].getWidth() / 2,posicaoInicialVerticalGoku + gokus[0].getHeight()/2,gokus[0].getWidth()/2);
+		//cima
+		shapeRenderer.rect(posicaoTorreHorizontal ,alturaDispositivo / 2 + espacoEntreTorre /2 + posicaoTorreVertical,
+				torreCima.getWidth(), torreCima.getHeight());
+		//baixo
+		shapeRenderer.rect(posicaoTorreHorizontal ,alturaDispositivo / 2 - torreBaixo.getHeight() - espacoEntreTorre/2 + posicaoTorreVertical,
+				torreBaixo.getWidth(), torreBaixo.getHeight());
+		shapeRenderer.end();
+
+		 */
 
 	private void detectarColisao(){
 
@@ -184,30 +199,25 @@ public class Game extends ApplicationAdapter {
 
 		}
 
-		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-		shapeRenderer.setColor(Color.RED);
-		shapeRenderer.circle(30 + gokus[1].getWidth() / 2,posicaoInicialVerticalGoku + gokus[1].getHeight()/2,gokus[1].getWidth()/2);
-		//cima
-		shapeRenderer.rect(posicaoTorreHorizontal ,alturaDispositivo / 2 + espacoEntreTorre /2 + posicaoTorreVertical,
-				torreCima.getWidth(), torreCima.getHeight());
-		//baixo
-		shapeRenderer.rect(posicaoTorreHorizontal ,alturaDispositivo / 2 - torreBaixo.getHeight() - espacoEntreTorre/2 + posicaoTorreVertical,
-				torreBaixo.getWidth(), torreBaixo.getHeight());
-		shapeRenderer.end();
-
 	}
 
 
 
 	private void desenharTexturas(){
-		//batch.setProjectionMatrix( camera.combined );
+		batch.setProjectionMatrix( camera.combined );
 		batch.begin();
 		batch.draw(fundo, 0,0, larguraDispositivo,alturaDispositivo);
+
 		batch.draw(gokus[(int) variação],30 + posiçaoHorizontalGoku, posicaoInicialVerticalGoku);
 		batch.draw(torreBaixo,posicaoTorreHorizontal ,alturaDispositivo / 2 - torreBaixo.getHeight() - espacoEntreTorre/2 + posicaoTorreVertical);
 		batch.draw(torreCima, posicaoTorreHorizontal ,alturaDispositivo / 2 + espacoEntreTorre /2 + posicaoTorreVertical);
-
 		textoPontuaçao.draw(batch, String.valueOf(pontos), larguraDispositivo/ 2, alturaDispositivo -60);
+
+		if (estadoJogo == 0){
+			batch.draw(fundo, 0,0, larguraDispositivo,alturaDispositivo);
+			batch.draw(play,larguraDispositivo/2 -play.getWidth()/2, alturaDispositivo/2 - play.getHeight()/2);
+			textoIniciar.draw(batch, "Toque para Iniciar", larguraDispositivo/2 - play.getWidth()/2, alturaDispositivo/2 + play.getHeight());
+		}
 
 		if (estadoJogo == 2){
 			batch.draw(gameOver, larguraDispositivo/2 - gameOver.getWidth()/2, alturaDispositivo/2);
@@ -220,15 +230,13 @@ public class Game extends ApplicationAdapter {
 
 	}
 	public void validarPontos(){
-		if (posicaoTorreHorizontal < 30 - gokus[0].getWidth()) {
+		if (posicaoTorreHorizontal < 50 - gokus[0].getWidth()) {
 			if (!passouTorre){
 				pontos++;
 				passouTorre = true;
 				somPontuaçao.play();
 			}
 		}
-
-
 		variação += Gdx.graphics.getDeltaTime() * 8;
 
 		if (variação > 3)
@@ -245,8 +253,7 @@ public class Game extends ApplicationAdapter {
 		fundo = new Texture("fundo.png");
 		torreBaixo = new Texture("torre_baixo.png");
 		torreCima = new Texture("torre_cima.png");
-		coin = new Texture("img_coin.png");
-
+		play = new Texture("play.png");
 		gameOver = new Texture("game_over.png");
 
 	}
@@ -254,23 +261,25 @@ public class Game extends ApplicationAdapter {
 		batch = new SpriteBatch();
 
 		random = new Random();
-		larguraDispositivo= Gdx.graphics.getWidth();
-		alturaDispositivo= Gdx.graphics.getHeight();
+		larguraDispositivo= VIRTUAL_WIDTH;
+		alturaDispositivo= VIRTUAL_HEIGHT;
 		posicaoInicialVerticalGoku = alturaDispositivo /2;
 		posicaoTorreHorizontal = larguraDispositivo;
-		espacoEntreTorre =  500;
-		posicaoMoeda = larguraDispositivo/2;
+		espacoEntreTorre =  400;
 
 		//configurações dos textos
 		textoPontuaçao = new BitmapFont();
-		textoPontuaçao.setColor(Color.GOLD);
-		textoPontuaçao.getData().setScale(6);
+		textoPontuaçao.setColor(Color.BLACK);
+		textoPontuaçao.getData().setScale(5);
 		textoReiniciar = new BitmapFont();
-		textoReiniciar.setColor(Color.GREEN);
-		textoReiniciar.getData().setScale(3);
+		textoReiniciar.setColor(Color.SCARLET);
+		textoReiniciar.getData().setScale(4);
+		textoIniciar = new BitmapFont();
+		textoIniciar.setColor(Color.ORANGE);
+		textoIniciar.getData().setScale(5);
 
 		textoMelhorPontuaçao = new BitmapFont();
-		textoMelhorPontuaçao.setColor(Color.RED);
+		textoMelhorPontuaçao.setColor(Color.NAVY);
 		textoMelhorPontuaçao.getData().setScale(3);
 
 		//Formas Geometricas para colisao
@@ -290,17 +299,15 @@ public class Game extends ApplicationAdapter {
 		pontuacaoMaxima = preferencia.getInteger("pontuaçaoMaxima", 0);
 
 		//Configuração da câmera
-		/*camera = new OrthographicCamera();
+		camera = new OrthographicCamera();
 		camera.position.set(VIRTUAL_WIDTH/2, VIRTUAL_HEIGHT/2, 0);
 		viewport = new StretchViewport(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, camera);
-
-		 */
 
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		//viewport.update(width, height);
+		viewport.update(width, height);
 	}
 
 	@Override
